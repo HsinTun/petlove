e99d8ffaefeaef00e9f6a0851f60685b228c139f
HsinTun <orange3130873@gmail.com>
2021-06-02 14:42:55 +0800

    細修

--- js/Booking/booking_woof.js
+++ js/Booking/booking_woof.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
 });
 $('.l_btn_dog').click(function () {
     $('.l_btn_cat').removeClass('button_color');
+    $('.l_btn_cat').removeClass('l_btn_cat2');
     $('.l_btn_dog').addClass('button_color');
     $('.l_dog_room').fadeIn(1000);
     $('.l_cat_room').hide();
@@ -26,8 +27,10 @@ $('.l_btn_cat').click(function () {
 
 // _mobile
 $('.l_btn_dog_mobile').click(function () {
+
     $('.l_btn_dog_mobile').css('background-color', '#F9DD83');
     $('.l_btn_cat_mobile').css('background-color', 'white');
+
 
     $('.l_dog_room_mobile').css('opacity', '1').css('transition', '.5s');
     $('.l_cat_room_mobile').hide();
